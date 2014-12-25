de3cea78cadd463820bc826bde43f0a9e19c1b65
alice <aliceworkmail@gmail.com>
2014-12-25 13:02:05 -0800

    added modal

---
+++ An Apple A Day/ChallengeSegue.h
@@ -0,0 +1,13 @@
+//
+//  ChallengeSegue.h
+//  An Apple A Day
+//
+//  Created by Alice Jia Qi Liu on 12/25/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChallengeSegue : UIStoryboardSegue
+
+@end
