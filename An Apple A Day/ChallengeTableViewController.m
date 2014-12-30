5097439e8105715e6b596983c1d52541288714d6
Kevin Casey <kacasey@berkeley.edu>
2014-12-29 19:18:40 -0800

    don't get destroyed by race condition

--- An Apple A Day/ChallengeTableViewController.m
+++ An Apple A Day/ChallengeTableViewController.m
@@ -77,6 +77,7 @@ Global *globalKeyValueStore;
         NSLog(@"today's challenge: %@", [self.challenge toString]);
         
         [self.tableView reloadData];
+        [self queryCompletedDailyChallenge];
     } onError:^(NSError *error, ResponseType type) {
         // query execution failed.
         // error.userinfo contains more details regarding the same
