6bd028f4b40620cea32f204523e22fc57c9ed0ba
Kevin Casey <kacasey@berkeley.edu>
2014-12-31 04:36:34 -0800

    scaffold website for beam

---
+++ website/app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websiteApp
+ */
+angular.module('websiteApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.email = 'kacasey@berkeley.edu';
+  });
