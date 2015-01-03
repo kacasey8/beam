2b34f9991b043f2aa890adf6e12cd31c5bc2d538
alice <aliceworkmail@gmail.com>
2015-01-02 22:51:18 -0800

    moved info to controller

--- website/app/scripts/controllers/main.js
+++ website/app/scripts/controllers/main.js
@@ -15,6 +15,21 @@ angular.module('websiteApp')
       'Karma'
     ];
 
+    $scope.features = [
+    	{
+    		'title': "Daily Challenges",
+    		'description': "Receive daily challenges to remind you of what's important in your life, such as your health, relationships, and leisure time - all integral to your happiness!"
+    	},
+    	{
+    		'title': "Track progress",
+    		'description': "Record your experience in completing the challenge each day through text, photo/video so you can look back and see what you've accomplished!"
+    	},
+    	{
+    		'title': "Get motivated",
+    		'description': "Get motivated to complete more challenges by seeing your current and streak highest as well as how many challenges you have completed!"
+    	}
+    ];
+
     $scope.submit = function(email) {
       console.log(email);
       // Built.Extension.execute('invite', {email: email}, {
